7e005cc528efb90a63c742072c40fe3e6719375f
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-10-04 15:00:56 -0400

    declare win works

--- scratch.js
+++ scratch.js
@@ -1,91 +1,117 @@
-let createBoard = () => {
-  let newDiv1 = document.createElement('div')
-  newDiv1.innerHTML = cardBack
-  board.append(newDiv1)
-  newDiv1.addEventListener('click', () => {
-    if ((newDiv1.innerHTML = cardBack)) {
-      newDiv1.innerHTML = randomCards[0].html
-    }
-  })
-  let newDiv2 = document.createElement('div')
-  newDiv2.innerHTML = cardBack
-  board.append(newDiv2)
-  newDiv2.addEventListener('click', () => {
-    if ((newDiv2.innerHTML = cardBack)) {
-      newDiv2.innerHTML = randomCards[1].html
-    }
-  })
-  let newDiv3 = document.createElement('div')
-  newDiv3.innerHTML = cardBack
-  board.append(newDiv3)
-  newDiv3.addEventListener('click', () => {
-    if ((newDiv3.innerHTML = cardBack)) {
-      newDiv3.innerHTML = randomCards[2].html
-    }
-  })
-  let newDiv4 = document.createElement('div')
-  newDiv4.innerHTML = cardBack
-  board.append(newDiv4)
-  newDiv4.addEventListener('click', () => {
-    if ((newDiv4.innerHTML = cardBack)) {
-      newDiv4.innerHTML = randomCards[3].html
-    }
-  })
-  let newDiv5 = document.createElement('div')
-  newDiv5.innerHTML = cardBack
-  board.append(newDiv5)
-  newDiv5.addEventListener('click', () => {
-    if ((newDiv5.innerHTML = cardBack)) {
-      newDiv5.innerHTML = randomCards[4].html
-    }
-  })
-  let newDiv6 = document.createElement('div')
-  newDiv6.innerHTML = cardBack
-  board.append(newDiv6)
-  newDiv6.addEventListener('click', () => {
-    if ((newDiv6.innerHTML = cardBack)) {
-      newDiv6.innerHTML = randomCards[5].html
-    }
-  })
+let newDiv = document.createElement('div')
+let newList = document.createElement('ul')
+newList.appendChild(newDiv)
+
+function enterPlayer() {
+  let player = document.getElementById('playerName')
+
+  // let newDiv = document.createElement('div')
+  // let newList = document.createElement('ul')
+  let listItem = document.createElement('li')
+
+  listItem.setAttribute('class', 'players')
+  newList.innerText = 'Players:'
+  listItem.innerText = player.value
+
+  newList.appendChild(listItem)
+
+  document.querySelector('.playerNames').appendChild(newDiv)
+
+  players.push(player.value)
+
+  return players
 }
-createBoard()
 
-// randomCards.forEach(() => {
-//   let createDiv = () => {
-//     let newDiv = document.createElement('div')
-//     newDiv.innerHTML =
-//     board.append(newDiv)
-//   }
-//   createDiv()
-// })
+addPlayerButton.addEventListener('click', enterPlayer)
 
-// let randomize = () => {
-//   let index = Math.floor(Math.random() * cards.length)
-//   randomCards.push(cards[index])
-//put into randomCards
-//take out of cards
-//splice index from cards
+// let createBoard = () => {
+//   let newDiv1 = document.createElement('div')
+//   newDiv1.innerHTML = cardBack
+//   board.append(newDiv1)
+//   newDiv1.addEventListener('click', () => {
+//     if ((newDiv1.innerHTML = cardBack)) {
+//       newDiv1.innerHTML = randomCards[0].html
+//     }
+//   })
+//   let newDiv2 = document.createElement('div')
+//   newDiv2.innerHTML = cardBack
+//   board.append(newDiv2)
+//   newDiv2.addEventListener('click', () => {
+//     if ((newDiv2.innerHTML = cardBack)) {
+//       newDiv2.innerHTML = randomCards[1].html
+//     }
+//   })
+//   let newDiv3 = document.createElement('div')
+//   newDiv3.innerHTML = cardBack
+//   board.append(newDiv3)
+//   newDiv3.addEventListener('click', () => {
+//     if ((newDiv3.innerHTML = cardBack)) {
+//       newDiv3.innerHTML = randomCards[2].html
+//     }
+//   })
+//   let newDiv4 = document.createElement('div')
+//   newDiv4.innerHTML = cardBack
+//   board.append(newDiv4)
+//   newDiv4.addEventListener('click', () => {
+//     if ((newDiv4.innerHTML = cardBack)) {
+//       newDiv4.innerHTML = randomCards[3].html
+//     }
+//   })
+//   let newDiv5 = document.createElement('div')
+//   newDiv5.innerHTML = cardBack
+//   board.append(newDiv5)
+//   newDiv5.addEventListener('click', () => {
+//     if ((newDiv5.innerHTML = cardBack)) {
+//       newDiv5.innerHTML = randomCards[4].html
+//     }
+//   })
+//   let newDiv6 = document.createElement('div')
+//   newDiv6.innerHTML = cardBack
+//   board.append(newDiv6)
+//   newDiv6.addEventListener('click', () => {
+//     if ((newDiv6.innerHTML = cardBack)) {
+//       newDiv6.innerHTML = randomCards[5].html
+//     }
+//   })
 // }
+// createBoard()
 
-// let createDiv = () => {
-//   let newDiv = document.createElement('div')
-//   newDiv.innerHTML = randomCards
-//   board.append(newDiv)
-// }
+// // randomCards.forEach(() => {
+// //   let createDiv = () => {
+// //     let newDiv = document.createElement('div')
+// //     newDiv.innerHTML =
+// //     board.append(newDiv)
+// //   }
+// //   createDiv()
+// // })
 
-// cards.forEach(() => {
-//   createDiv()
-// })
+// // let randomize = () => {
+// //   let index = Math.floor(Math.random() * cards.length)
+// //   randomCards.push(cards[index])
+// //put into randomCards
+// //take out of cards
+// //splice index from cards
+// // }
 
-//math.random from random cards to innerhtml reassignment to get the whole sheband to loop
-//make grid size cards *2
-//for each item item in random cards array generate div
-//after two clicks check to see if clicked divs are ===
-//if yes, nothing, if no, flip
+// // let createDiv = () => {
+// //   let newDiv = document.createElement('div')
+// //   newDiv.innerHTML = randomCards
+// //   board.append(newDiv)
+// // }
 
-// const onClick1 = () => {
-//   if (newDiv1.innerHTML !== newDiv2.innerHTML) {
-//     newDiv1.innerHTML = cardBack
-//     newDiv2.innerHTML = cardBack
-//   }
-// }
+// // cards.forEach(() => {
+// //   createDiv()
+// // })
+
+// //math.random from random cards to innerhtml reassignment to get the whole sheband to loop
+// //make grid size cards *2
+// //for each item item in random cards array generate div
+// //after two clicks check to see if clicked divs are ===
+// //if yes, nothing, if no, flip
+
+// // const onClick1 = () => {
+// //   if (newDiv1.innerHTML !== newDiv2.innerHTML) {
+// //     newDiv1.innerHTML = cardBack
+// //     newDiv2.innerHTML = cardBack
+// //   }
+// // }
--- scripts.js
+++ scripts.js
@@ -1,15 +1,10 @@
-console.log(`Country roads`)
-console.log(`Let's a-gooooo`)
-console.log("It's a-meeeeee")
-console.log('MARIOOOOOOOOO!!!!!')
-
 const board = document.querySelector('.board')
 const cardBack = '<img src="https://i.imgur.com/ywruWIz.jpg">'
 const cards = [
-  { name: 'coin10', html: '<img src = "https://i.imgur.com/E7dMsON.jpeg">' },
-  { name: 'coin20', html: '<img src = "https://i.imgur.com/krrXWfU.jpeg">' },
-  { name: 'flower', html: '<img src = "https://i.imgur.com/rUjl6lP.jpeg">' },
-  { name: 'life', html: '<img src="https://i.imgur.com/T5hg4TQ.jpeg">' },
+  // { name: 'coin10', html: '<img src = "https://i.imgur.com/E7dMsON.jpeg">' },
+  // { name: 'coin20', html: '<img src = "https://i.imgur.com/krrXWfU.jpeg">' },
+  // { name: 'flower', html: '<img src = "https://i.imgur.com/rUjl6lP.jpeg">' },
+  // { name: 'life', html: '<img src="https://i.imgur.com/T5hg4TQ.jpeg">' },
   { name: 'mushroom', html: '<img src="https://i.imgur.com/G08DFIg.jpeg">' },
   { name: 'star', html: '<img src="https://i.imgur.com/daLHV0t.jpeg">' }
 ]
@@ -19,6 +14,7 @@ let secondCard = null
 let firstDiv = null
 let secondDiv = null
 let score = 0
+// let lock = false
 
 cards.forEach((card) => {
   randomCards.splice(
@@ -42,6 +38,7 @@ randomCards.forEach((card) => {
 })
 
 const flipCard = (div, card) => {
+  // if (lock) return
   if (div.innerHTML === cardBack) {
     div.innerHTML = card.html
     div.classList.remove('back')
@@ -50,12 +47,12 @@ const flipCard = (div, card) => {
     if (!firstCard) {
       firstCard = card
       firstDiv = div
-      console.log('first card created')
     } else {
       secondCard = card
       secondDiv = div
-      console.log('second card created')
+      // lock = true
       compareCard()
+      declareWin()
     }
   }
 }
@@ -70,14 +67,14 @@ const compareCard = () => {
     setTimeout(() => {
       firstDiv.innerHTML = cardBack
       secondDiv.innerHTML = cardBack
-    }, 1000)
+    }, 500)
     firstCard = null
     secondCard = null
   }
 }
 
 const declareWin = () => {
-  if ((score = randomCards.length / 2)) {
+  if (score === randomCards.length / 2) {
     console.log('win!')
   }
 }
