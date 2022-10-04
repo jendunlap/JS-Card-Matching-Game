5e318af5ff05a738679cab8ab0aa8cae5969d32b
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-10-04 14:02:02 -0400

    game play actually works

--- scripts.js
+++ scripts.js
@@ -6,14 +6,13 @@ console.log('MARIOOOOOOOOO!!!!!')
 const board = document.querySelector('.board')
 const cardBack = '<img src="https://i.imgur.com/ywruWIz.jpg">'
 const cards = [
-  // { name: 'coin10', html: '<img src = "https://i.imgur.com/E7dMsON.jpeg">' },
-  // { name: 'coin20', html: '<img src = "https://i.imgur.com/krrXWfU.jpeg">' },
-  // { name: 'flower', html: '<img src = "https://i.imgur.com/rUjl6lP.jpeg">' },
+  { name: 'coin10', html: '<img src = "https://i.imgur.com/E7dMsON.jpeg">' },
+  { name: 'coin20', html: '<img src = "https://i.imgur.com/krrXWfU.jpeg">' },
+  { name: 'flower', html: '<img src = "https://i.imgur.com/rUjl6lP.jpeg">' },
   { name: 'life', html: '<img src="https://i.imgur.com/T5hg4TQ.jpeg">' },
   { name: 'mushroom', html: '<img src="https://i.imgur.com/G08DFIg.jpeg">' },
   { name: 'star', html: '<img src="https://i.imgur.com/daLHV0t.jpeg">' }
 ]
-//if score === randomCards.length%2 declare win
 let randomCards = []
 let firstCard = null
 let secondCard = null
@@ -34,6 +33,33 @@ cards.forEach((card) => {
   )
 })
 
+randomCards.forEach((card) => {
+  let cardDiv = document.createElement('div')
+  cardDiv.innerHTML = cardBack
+  cardDiv.classList.add('back')
+  board.append(cardDiv)
+  cardDiv.addEventListener('click', () => flipCard(cardDiv, card))
+})
+
+const flipCard = (div, card) => {
+  if (div.innerHTML === cardBack) {
+    div.innerHTML = card.html
+    div.classList.remove('back')
+    div.classList.add(card.name)
+    div.classList.add('flipped')
+    if (!firstCard) {
+      firstCard = card
+      firstDiv = div
+      console.log('first card created')
+    } else {
+      secondCard = card
+      secondDiv = div
+      console.log('second card created')
+      compareCard()
+    }
+  }
+}
+
 const compareCard = () => {
   if (firstCard === secondCard) {
     score++
@@ -45,31 +71,14 @@ const compareCard = () => {
       firstDiv.innerHTML = cardBack
       secondDiv.innerHTML = cardBack
     }, 1000)
+    firstCard = null
+    secondCard = null
   }
 }
 
-const flipCard = (div, card) => {
-  if (div.innerHTML === cardBack) {
-    div.innerHTML = card.html
-    div.classList.add(card.name)
-    div.classList.add('flipped')
-    if (!firstCard) {
-      firstCard = card
-      firstDiv = div
-      //if there IS NO FIRST CARD, assign it
-    } else {
-      secondCard = card
-      secondDiv = div
-      compareCard()
-      //if there IS a first card, assign second card
-    }
+const declareWin = () => {
+  if ((score = randomCards.length / 2)) {
+    console.log('win!')
   }
 }
-
-randomCards.forEach((card) => {
-  let cardDiv = document.createElement('div')
-  cardDiv.innerHTML = cardBack
-  cardDiv.classList.add('back')
-  board.append(cardDiv)
-  cardDiv.addEventListener('click', () => flipCard(cardDiv, card))
-})
+declareWin()
