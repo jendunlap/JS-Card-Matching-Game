3ff754069743fffce3eb10baf258ac0a5552b2d9
Jen Dunlap <jenniferleighdunlap@gmail.com>
2022-10-05 11:28:35 -0400

    created footer and footer content

--- scripts.js
+++ scripts.js
@@ -68,6 +68,7 @@ const compareCard = () => {
   if (firstCard.name === secondCard.name) {
     lock = false
     score++
+    console.log(score)
   } else {
     setTimeout(() => {
       firstDiv.innerHTML = cardBack
@@ -84,3 +85,43 @@ const declareWin = () => {
     console.log('win!')
   }
 }
+
+//////FOOTER//////
+const footer = document.querySelector('.footer')
+const scoreBoard = document.createElement('div')
+scoreBoard.classList.add('scoreboard')
+footer.append(scoreBoard)
+
+const topRow = document.createElement('div')
+topRow.classList.add('toprow')
+scoreBoard.append(topRow)
+const world = document.createElement('div')
+world.classList.add('world')
+const p = document.createElement('div')
+p.classList.add('p')
+const scoreCount = document.createElement('div')
+scoreCount.classList.add('scorecount')
+topRow.append(world)
+topRow.append(p)
+topRow.append(scoreCount)
+
+world.innerText = 'WORLD 8'
+p.innerText = 'ᐅ ᐅ ᐅ ᐅ ᐅ ᐅ ᐅ P'
+scoreCount.innerText = '🟡 ' + score
+
+const bottomRow = document.createElement('div')
+bottomRow.classList.add('bottomrow')
+scoreBoard.append(bottomRow)
+const lives = document.createElement('div')
+lives.classList.add('lives')
+const numbers = document.createElement('div')
+numbers.classList.add('numbers')
+const timeDisplay = document.createElement('div')
+timeDisplay.classList.add('timedisplay')
+bottomRow.append(lives)
+bottomRow.append(numbers)
+bottomRow.append(timeDisplay)
+
+lives.innerText = 'M x 001'
+numbers.innerText = '4 2 0 6 9 6 9'
+timeDisplay.innerText = '🕒 300'
